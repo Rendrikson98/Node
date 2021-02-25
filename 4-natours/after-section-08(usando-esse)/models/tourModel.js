f38b13a27b648f9a8a866ae5fc636dd8581c9c29
Rendrikson Oliveira <rendrikson16@gmail.com>
2021-02-24 21:55:18 -0300

    As vezes precisamos voltar alguns passos para poder seguir na direção certa

--- 4-natours/after-section-08(usando-esse)/models/tourModel.js
+++ 4-natours/after-section-08(usando-esse)/models/tourModel.js
@@ -18,10 +18,10 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a duration']
     },
-    maxGroupSize: {
+    /*maxGroupSize: {
       type: Number,
       required: [true, 'A tour must have a group size']
-    },
+    },*/
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
@@ -30,7 +30,7 @@ const tourSchema = new mongoose.Schema(
         message: 'Difficulty is either: easy, medium, difficult'
       }
     },
-    ratingsAverage: {
+    /*ratingsAverage: {
       type: Number,
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
@@ -39,11 +39,12 @@ const tourSchema = new mongoose.Schema(
     ratingsQuantity: {
       type: Number,
       default: 0
-    },
+    },*/
     price: {
       type: Number,
       required: [true, 'A tour must have a price']
-    },
+    }
+    /*,
     priceDiscount: {
       type: Number,
       validate: {
@@ -77,7 +78,7 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    }*/
   },
   {
     toJSON: { virtuals: true },
@@ -85,12 +86,12 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
-tourSchema.virtual('durationWeeks').get(function() {
+tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
-tourSchema.pre('save', function(next) {
+tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
@@ -107,20 +108,20 @@ tourSchema.pre('save', function(next) {
 
 // QUERY MIDDLEWARE
 // tourSchema.pre('find', function(next) {
-tourSchema.pre(/^find/, function(next) {
+tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
 
   this.start = Date.now();
   next();
 });
 
-tourSchema.post(/^find/, function(docs, next) {
+tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function(next) {
+tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 
   console.log(this.pipeline());
