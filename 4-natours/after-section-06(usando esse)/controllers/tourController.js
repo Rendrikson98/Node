1e507950a2774f71e9e9b873282ea373f72f7d1a
Rendrikson Oliveira <rendrikson16@gmail.com>
2021-04-30 11:30:10 -0300

    adding the filter the sort, field and pagination, also the watched up to lecture 98

--- 4-natours/after-section-06(usando esse)/controllers/tourController.js
+++ 4-natours/after-section-06(usando esse)/controllers/tourController.js
@@ -14,23 +14,50 @@ exports.getAllTours = async (req, res) => {
       .equals('easy');*/
 
     //BUILD QUERY 
-    //1) Filtering
+    //1A) Filtering
     const queryObj = { ...req.query };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach(el => delete queryObj[el]);
 
-    console.log(queryObj);
-
-    //2) Advanced filtering
+    //1B) Advanced filtering
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    console.log(JSON.parse(queryStr))
+    let query = Tour.find(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    //2)Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt')
+    }
+    //3) fields limit
+    if (req.query.fields) {
+      const fieldsBy = req.query.fields.split(',').join(' ');
+      query = query.select(fieldsBy)
+    } else {
+      query = query.select('-__v')
+    }
+
+    //4) Pagination
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //EXECUTE QUERY
     const tours = await query;
+    // query.sort().select().skip(),limit()
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
